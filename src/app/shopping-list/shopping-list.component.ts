d48337f116b22f7de6c76d95b832e6490a2d7a3d
sajag16642 <sajag16642gmail.com>
2019-06-28 14:26:24 +0530

    components setup

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredients.model'
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+
+  ingredients : Ingredient[] =[
+    new Ingredient('Appple',5),
+    new Ingredient('tomatoes',10)
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
